d28280b5d9b3ceef5f2a9633a5eb9552568576cb
daw2 <daw2@IF04-10>
2026-09-22 19:20:13 +0200

    Prueba2

--- src/gestorarchivos/manuelgonzalezencinas/GestorArchivosManuelGonzalezEncinas.java
+++ src/gestorarchivos/manuelgonzalezencinas/GestorArchivosManuelGonzalezEncinas.java
@@ -1,6 +1,9 @@
 package gestorarchivos.manuelgonzalezencinas;
 public class GestorArchivosManuelGonzalezEncinas {
     public static void main(String[] args) {
+        
+        System.out.println("Hola cabrones");
+        
     }
     
 }
